c0806b7308852cc29f74006d7bfb28b605d6e813
Ben Skinner <bms41@cam.ac.uk>
2015-03-24 19:33:10 +0000

    Added ProfileException, used in the ProfileAggregate

---
+++ src/no/exceptions/ProfileException.java
@@ -0,0 +1,15 @@
+package no.exceptions;
+
+public class ProfileException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ProfileException() { super(); }
+	public ProfileException(String message) { super(message); }
+	public ProfileException(String message, Throwable cause) { super(message, cause); }
+	public ProfileException(Throwable cause) { super(cause); }
+
+
+}
