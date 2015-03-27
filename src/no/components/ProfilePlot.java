b9350a84787820cbd841f72f28a040e545208be0
Ben Skinner <bms41@cam.ac.uk>
2015-03-27 16:11:39 +0000

    Added export function to ProfilePlot. Not currently in use.

--- src/no/components/ProfilePlot.java
+++ src/no/components/ProfilePlot.java
@@ -1,9 +1,13 @@
 package no.components;
 
+import ij.IJ;
+import ij.ImagePlus;
 import ij.gui.Plot;
+import ij.measure.Calibration;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 // holds e.g. the normalised and raw plots
 public class ProfilePlot {
@@ -20,5 +24,18 @@ public class ProfilePlot {
 	public Plot get(String s){
 		return this.collection.get(s);
 	}
+	
+	public Set<String> getTypes(){
+		return this.collection.keySet();
+	}
+	
+	public void export(String plotName, String exportName){
+	    ImagePlus image = this.get(plotName).getImagePlus();
+	    Calibration cal = image.getCalibration();
+	    cal.setUnit("pixels");
+	    cal.pixelWidth = 1;
+	    cal.pixelHeight = 1;
+	    IJ.saveAsTiff(image, exportName+"."+plotName+".tiff");
+	  }
 
 }
