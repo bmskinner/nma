b1db10ff2973bea699f2eb16cd338dfb50b0ccde
ben <bms41@cam.ac.uk>
2015-06-05 14:59:48 +0100

    Added placeholder for Dapi Densitometer

---
+++ src/no/analysis/DapiDensitometer.java
@@ -0,0 +1,22 @@
+package no.analysis;
+
+import java.util.List;
+
+public class DapiDensitometer {
+
+	
+	public static boolean run(List<AnalysisDataset> list){
+		
+//		for each nucleus
+		// get com
+		// get tail
+		// get line from com-tail
+		// get line at increment degrees to border
+		// make roi
+		// measure channel 1 density as % of total density
+		
+		
+		
+		return false;
+	}
+}
