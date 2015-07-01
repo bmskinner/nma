e3028c1926a4c816ff94c9c7703311e1b74634ad
Ben Skinner <bms41@cam.ac.uk>
2015-07-01 15:01:20 +0100

    Added file to hold tail detection prototype

---
+++ src/no/analysis/TubulinTailDetector.java
@@ -0,0 +1,30 @@
+package no.analysis;
+
+
+
+/**
+ * This class is to test ideas for detecting sperm tails stained with
+ * anti-tubulin
+ */
+public class TubulinTailDetector {
+	
+	
+	// import image with tubulin in green channel
+	
+	// edge / threshold to find tubulin stain
+	
+	// create a binary mask of region
+	
+	// skeletonise
+	
+	// detect particles area 0
+	
+	// check each roi: print outline
+	
+	// convert area roi to line roi if needed
+	
+	// create line selection
+	
+	// straighten / measure
+
+}
