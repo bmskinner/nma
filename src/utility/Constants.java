ca8d67d393bc9d68508e818faaec9fc63a9ad38a
Ben Skinner <bms41@cam.ac.uk>
2015-11-05 09:50:24 +0000

    Prep for 1.11.3

--- src/utility/Constants.java
+++ src/utility/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
 	 */
 	public static final int VERSION_MAJOR    = 1;
 	public static final int VERSION_REVISION = 11;
-	public static final int VERSION_BUGFIX   = 2;
+	public static final int VERSION_BUGFIX   = 3;
 	
 	// nmd = Nuclear Morphology Dataset
 	public static final String SAVE_FILE_EXTENSION = ".nmd";
