4964aaea1039959a3b3e21daca1af03dc3867d56
Ben Skinner <bms41@cam.ac.uk>
2016-01-22 12:22:39 +0000

    Fixed copyrights

--- src/gui/tabs/NucleusMagnitudePanel.java
+++ src/gui/tabs/NucleusMagnitudePanel.java
@@ -1,11 +1,26 @@
+/*******************************************************************************
+ *  	Copyright (C) 2016 Ben Skinner
+ *   
+ *     This file is part of Nuclear Morphology Analysis.
+ *
+ *     Nuclear Morphology Analysis is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Nuclear Morphology Analysis is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Nuclear Morphology Analysis. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package gui.tabs;
 
 import gui.components.ExportableTable;
 import gui.components.PairwiseTableCellRenderer;
 
-import java.awt.Color;
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,7 +31,6 @@ import javax.swing.SwingUtilities;
 import javax.swing.table.TableModel;
 
 import stats.NucleusStatistic;
-import utility.Constants;
 import charting.NucleusStatsTableOptions;
 import charting.TableOptions;
 import charting.datasets.NucleusTableDatasetCreator;
