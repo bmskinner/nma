75e651630728a8137411a971d2735501121dc9d3
Ben Skinner <bms41@cam.ac.uk>
2016-01-26 09:30:56 +0000

    Reverted FISH remapping to working version from 1.11.5.

--- src/gui/dialogs/FishRemappingDialog.java
+++ src/gui/dialogs/FishRemappingDialog.java
@@ -19,58 +19,31 @@
 package gui.dialogs;
 
 import gui.ImageType;
-import gui.LoadingIconDialog;
 import gui.MainWindow;
-import gui.components.PaintableJPanel;
-import ij.IJ;
-import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.PolygonRoi;
-import ij.gui.Roi;
 import ij.io.DirectoryChooser;
 import ij.process.FloatPolygon;
 import ij.process.ImageProcessor;
 import io.ImageExporter;
 import io.ImageImporter;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.event.MouseInputAdapter;
-
 import utility.Utils;
 import analysis.AnalysisDataset;
-import analysis.nucleus.SignalFinder;
 import components.Cell;
 import components.CellCollection;
-import components.nuclear.NuclearSignal;
 import components.nuclei.Nucleus;
 
 @SuppressWarnings("serial")
@@ -78,24 +51,13 @@ public class FishRemappingDialog extends ImageProber {
 
 	public static final int NUCLEUS_OUTLINE_WIDTH = 2;
 	
-	private final AnalysisDataset dataset;
+	private AnalysisDataset dataset;
 	private File postFISHImageDirectory;
 		
 	private List<UUID> selectedNucleiLeft = new ArrayList<UUID>(0);
 	private List<UUID> selectedNucleiRight = new ArrayList<UUID>(0);
-	
-	private int offsetX = 0;
-	private int offsetY = 0;
-	
-//	private PaintableJPanel paintablePanel;
-//	private Image originalImageZoom;
 		
 	public enum FishMappingImageType implements ImageType {
-		
-//		ORIGINAL_IMAGE   ("Original image"),
-//		FISH_IMAGE		 ("FISHed image"),
-//		ORIGINAL_IMAGE_LARGE   ("Full scale original"),
-//		FISH_IMAGE_LARGE   ("Full scale FISH");
 		
 		ORIGINAL_IMAGE   ("Original image"),
 		FISH_IMAGE		 ("FISHed image");
@@ -118,135 +80,71 @@ public class FishRemappingDialog extends ImageProber {
 	/**
 	 * Create the dialog.
 	 */
-	public FishRemappingDialog(final MainWindow mw, final AnalysisDataset dataset, final Logger programLogger) {
+	public FishRemappingDialog(MainWindow mw, AnalysisDataset dataset, Logger programLogger) {
 		
 		super(dataset.getAnalysisOptions(), programLogger, FishMappingImageType.ORIGINAL_IMAGE, dataset.getAnalysisOptions().getFolder());
 		
 		this.setTitle("FISH remapping");
-		this.setHeaderText("Detected nuclei are outlined in yellow. Left or right click to add nuclei to new collections. Ctrl-click the fish image to offset the zoomed region");
-		
-		this.setCancelButtonText("Cancel");
 		
 		// set the collectio of pre-FISH images
 		this.dataset = dataset;
 		
 		// Clear the 'large image' mouse listener
 		// It must be replaced with a custom listener
-		final ImageType originalImage = FishMappingImageType.ORIGINAL_IMAGE;
-		
-		for(MouseListener l : iconMap.get(originalImage).getMouseListeners()){
-			iconMap.get(originalImage).removeMouseListener(l);
+		for(MouseListener l : iconMap.get(FishMappingImageType.ORIGINAL_IMAGE).getMouseListeners()){
+			iconMap.get(FishMappingImageType.ORIGINAL_IMAGE).removeMouseListener(l);
 		}
 		
-		iconMap.get(originalImage).addMouseListener(new MouseAdapter() {
-			
+		iconMap.get(FishMappingImageType.ORIGINAL_IMAGE).addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
+
+//				getProgramLogger().log(Level.FINER, "Mouse clicked");
+//				 correct scaling 
+				ImageProcessor openProcessor = procMap.get(FishMappingImageType.ORIGINAL_IMAGE);
 				
-				Point originalPoint = convertIconLocationToOriginalImage(originalImage, e.getPoint());
+				double labelWidth = iconMap.get(FishMappingImageType.ORIGINAL_IMAGE).getWidth();
+				double labelHeight = iconMap.get(FishMappingImageType.ORIGINAL_IMAGE).getHeight();
+				
+				double iconWidth = iconMap.get(FishMappingImageType.ORIGINAL_IMAGE).getIcon().getIconWidth();
+				double iconHeight = iconMap.get(FishMappingImageType.ORIGINAL_IMAGE).getIcon().getIconHeight();
+				
+				double conversion = (double) iconWidth / (double) openProcessor.getWidth();
+				
+				
+				double x = e.getX() ; // positions on label
+				double y = e.getY() ;
+				
+				// convert to positions on the image icon
+				double xIcon = x - (  (labelWidth  - iconWidth ) /2 );
+				double yIcon = y - (  (labelHeight - iconHeight) /2 );;
+				int originalX =  (int) ( xIcon / conversion) ;
+				int originalY =  (int) ( yIcon / conversion) ;
+				
+//				getProgramLogger().log(Level.FINEST, "x: "+x);
+//				getProgramLogger().log(Level.FINEST, "y: "+y);
+//				getProgramLogger().log(Level.FINEST, "orignal x: "+originalX);
+//				getProgramLogger().log(Level.FINEST, "orignal y: "+originalY);
 
 				List<Cell> imageNuclei = FishRemappingDialog.this.dataset.getCollection().getCells(openImage);
-				
 				for(Cell c : imageNuclei){
 					Nucleus n = c.getNucleus();
+					double[] positions = n.getPosition();
 
-					FloatPolygon polygon = Utils.createPolygon(n.getOriginalBorderList());
+					FloatPolygon polygon = Utils.createPolygon(n.getBorderList());
+					PolygonRoi roi = new PolygonRoi(polygon, PolygonRoi.POLYGON);
+					roi.setLocation(positions[Nucleus.X_BASE], positions[Nucleus.Y_BASE]);
 
-					if(polygon.contains(originalPoint.x,  originalPoint.y)){
-						respondToMouseEvent(e, c, procMap.get(originalImage));
+					if(roi.contains(originalX, originalY)){
+//						getProgramLogger().log(Level.FINER, "Nucleus clicked");
+						respondToMouseEvent(e, c, openProcessor);
 					}
 				}
-				
 				updateImageThumbnails();
 			}
 		});
 		
-//		iconMap.get(originalImage).addMouseMotionListener(new MouseAdapter(){
-//			@Override
-//			public void mouseMoved(MouseEvent e){
-//				
-//				Point location = e.getPoint();
-//				Point originalPoint = convertIconLocationToOriginalImage(originalImage, location);
-//
-//				int w = 400;
-//				int border = w/2;
-//				/*
-//				 * Get a rectangle around the location
-//				 * Crop this section from the original image
-//				 * Set the large icons to use the cropped images
-//				 */
-//				
-//				int rX = originalPoint.x < border // if the point is less than the border distance from the left edge 
-//						? 0                       // set the rectangle edge to zero
-//						: originalPoint.x-border; // put the point a border distance to the left of the point
-////								                   otherwise
-////						: originalPoint.x > iconMap.get(originalImage).getWidth()-w // if the point is less than the border from teh right edge
-////							? iconMap.get(originalImage).getWidth()-w // set the rectangle edge to the right edge - the full width of the rectangle
-////									                          // otherwise
-////									: originalPoint.x-border; // put the point a border distance to the left of the point
-////							
-//							
-//				int rY = originalPoint.y < border 
-//						? 0 
-//						: originalPoint.y-border;
-////						: originalPoint.y > iconMap.get(originalImage).getHeight()-w
-////								? iconMap.get(originalImage).getHeight()-w
-////								: originalPoint.y-border;
-//				
-//				Rectangle r = new Rectangle(rX, rY, w, w);
-//				
-//				// The original image
-////				makeCroppedVersion(FishMappingImageType.ORIGINAL_IMAGE, FishMappingImageType.ORIGINAL_IMAGE_LARGE, r);
-//
-//				
-//				// The fish image
-//				Rectangle offsetR = new Rectangle(rX+(offsetX/2), rY+(offsetY/2), w, w);
-////				makeCroppedVersion(FishMappingImageType.FISH_IMAGE, FishMappingImageType.FISH_IMAGE_LARGE, offsetR);
-//								
-//				updateImageThumbnails();
-////				paintablePanel.setClip(offsetR);
-//				
-//			}
-//		});
 		
-		// Clear the 'large FISH image' mouse listener
-		// It must be replaced with a custom listener
-		final ImageType fishImage = FishMappingImageType.FISH_IMAGE;
-
-		for(MouseListener l : iconMap.get(fishImage).getMouseListeners()){
-			iconMap.get(fishImage).removeMouseListener(l);
-		}
-		
-//		iconMap.get(fishImage).addMouseListener(new MouseAdapter() {
-//			
-//			@Override
-//			public void mousePressed(MouseEvent e) {
-//				
-//				if(e.isControlDown() && e.getButton()==MouseEvent.BUTTON1){
-//					
-//					Point originalPoint = convertIconLocationToOriginalImage(fishImage, e.getPoint());
-//					
-//					programLogger.log(Level.INFO, "Offset FISH to "+originalPoint.x + "-"+ originalPoint.y);
-//					
-//					// Get the middle of the FISH image
-//					int midX = procMap.get(fishImage).getWidth()  / 2;
-//					int midY = procMap.get(fishImage).getHeight() / 2;
-//										
-//					offsetX = originalPoint.x - midX;
-//					offsetY = originalPoint.y - midY;
-//					
-//					updateImageThumbnails();
-//				}
-//				
-//			}
-//		});
-		
-		
-//		paintablePanel = new PaintableJPanel();
-//		paintablePanel.setMaximumSize(new Dimension(400, 400));
-//		paintablePanel.setMinimumSize(new Dimension(400, 400));
-////		
-//		this.add(paintablePanel, BorderLayout.NORTH);
 
 		// ask for a folder of post-FISH images
 		if(this.getPostFISHDirectory()){
@@ -258,16 +156,6 @@ public class FishRemappingDialog extends ImageProber {
 		}
 		
 	}
-	
-//	private void makeCroppedVersion(ImageType imageType, ImageType storageType, Rectangle r){
-//		// The original image
-//		ImageProcessor original = procMap.get(imageType).duplicate();
-//		procMap.put(storageType, null); // remove previous version
-//		original.setRoi(r);
-//		ImageProcessor crop = original.crop();
-//		procMap.put(storageType, crop);
-//		original = null; // free memory
-//	}
 	
 	/**
 	 * Get a list of CellCollections, containing the selected nuclei.
@@ -296,74 +184,40 @@ public class FishRemappingDialog extends ImageProber {
 		}
 		return result;
 	}
-	
-	/**
-	 * Import the morphology image file and make a greyscale image
-	 * @param imageFile
-	 */
-	private void importOriginalImage(final File imageFile){
-		ImageStack stack = ImageImporter.importImage(imageFile, programLogger);
-
-		programLogger.log(Level.FINEST, "Converting image");
-		ImageProcessor openProcessor = ImageExporter.makeGreyRGBImage(stack).getProcessor();
-		openProcessor.invert();
-		
-		// Get the cells matching the imageFile
-		for(Cell c : dataset.getCollection().getCells(imageFile)){
-			drawNucleus(c, openProcessor);
-		}
-		procMap.put(FishMappingImageType.ORIGINAL_IMAGE, openProcessor);
-	}
-	
-	/**
-	 * Get the file name from the input, and import the corresponding
-	 * file from the FISH image directory
-	 * @param imageFile
-	 */
-	private void importFISHImage(final File imageFile){
-		// Import the image as a stack
-		String imageName = imageFile.getName();
-		File fishImageFile = new File(postFISHImageDirectory+File.separator+imageName);
-
-		if(fishImageFile.exists()){
-			ImageStack fishStack = ImageImporter.importImage(fishImageFile, programLogger);
-
-			ImageProcessor fishProcessor = ImageExporter.convertToRGB(fishStack).getProcessor();
-
-			procMap.put(FishMappingImageType.FISH_IMAGE, fishProcessor);
-
-		} else {
-
-			/*
-			 * If there is no corresponding FISH image, don't display anything
-			 */
-			procMap.put(FishMappingImageType.FISH_IMAGE, null);
-		}
-	}
 		
 	
 	@Override
-	protected void importAndDisplayImage(final File imageFile){
+	protected void importAndDisplayImage(File imageFile){
 
 		try{
 			setStatusLoading();
 			this.setLoadingLabelText("Opening image "+index+": "+imageFile.getAbsolutePath()+"...");
 
+			ImageStack stack = ImageImporter.importImage(imageFile, programLogger);
+
+			// Import the image as a stack
+			String imageName = imageFile.getName();
+
+			programLogger.log(Level.FINEST, "Converting image");
+			ImageProcessor openProcessor = ImageExporter.makeGreyRGBImage(stack).getProcessor();
+			openProcessor.invert();
+			procMap.put(FishMappingImageType.ORIGINAL_IMAGE, openProcessor);
+
+						
+			File fishImageFile = new File(postFISHImageDirectory+File.separator+imageName);
+			ImageStack fishStack = ImageImporter.importImage(fishImageFile, programLogger);
 			
-			importOriginalImage(imageFile);
+			ImageProcessor fishProcessor = ImageExporter.convertToRGB(fishStack).getProcessor();
+			
+			procMap.put(FishMappingImageType.FISH_IMAGE, fishProcessor);
+			
+			// Get the cells matching the imageFile
+			for(Cell c : dataset.getCollection().getCells(imageFile)){
+				drawNucleus(c, openProcessor);
+			}
 
 			
-			importFISHImage(imageFile);
-			
-			
-//			makeCroppedVersion(FishMappingImageType.ORIGINAL_IMAGE, FishMappingImageType.ORIGINAL_IMAGE_LARGE, r);
-//			makeCroppedVersion(FishMappingImageType.FISH_IMAGE, FishMappingImageType.FISH_IMAGE_LARGE, r);
 
-			
-//			paintablePanel = new PaintableJPanel(openProcessor.getBufferedImage());
-//			paintablePanel.setMinimumSize(new Dimension(400,400));
-//			paintablePanel.setMaximumSize(new Dimension(400,400));
-			
 			updateImageThumbnails();
 
 			this.setLoadingLabelText("Showing nuclei in "+imageFile.getAbsolutePath());
