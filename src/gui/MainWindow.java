d80b7fae0745c6afb9d8cae37f38036b5f0a32d1
unknown <bms41@cam.ac.uk>
2016-01-31 13:10:12 +0000

    Cleaned code

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -56,8 +56,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
