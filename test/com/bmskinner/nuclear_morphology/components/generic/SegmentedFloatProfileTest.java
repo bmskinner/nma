9a236c3b05e0e1241b365118b23ae598184e2941
Ben Skinner <bms41@cam.ac.uk>
2018-02-26 17:04:17 +0000

    Added tests for profiles

--- test/com/bmskinner/nuclear_morphology/components/generic/SegmentedFloatProfileTest.java
+++ test/com/bmskinner/nuclear_morphology/components/generic/SegmentedFloatProfileTest.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -40,79 +41,71 @@ import com.bmskinner.nuclear_morphology.components.nuclear.IBorderSegment;
 import com.bmskinner.nuclear_morphology.components.nuclear.IBorderSegment.SegmentUpdateException;
 
 public class SegmentedFloatProfileTest {
-	
-//	static List<IBorderSegment> list = new ArrayList<IBorderSegment>();
-//	
-//	static {
-//		
-//		DefaultBorderSegment p1 = new DefaultBorderSegment(10, 20, 100);
-//		DefaultBorderSegment p2 = new DefaultBorderSegment(20, 45, 100);
-//		DefaultBorderSegment p3 = new DefaultBorderSegment(45, 89, 100);
-//		DefaultBorderSegment p4 = new DefaultBorderSegment(89, 10, 100);
-//
-//		list.add(p1);
-//		list.add(p2);
-//		list.add(p3);
-//		list.add(p4);
-//
-//		try {
-//			IBorderSegment.linkSegments(list);
-//		} catch (ProfileException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-	
+    
+    private static final String SEG_0 = "00000000-0000-0000-0000-000000000000";
+    private static final String SEG_1 = "11111111-1111-1111-1111-111111111111";
+    private static final String SEG_2 = "22222222-2222-2222-2222-222222222222";
+    private static final String SEG_3 = "33333333-3333-3333-3333-333333333333";
+        
+    private ISegmentedProfile sp;
+    
+    @Before
+    public void setUp() throws ProfileException{
+        sp = makeTestProfile();
+    }
+		
 	/**
 	 * Create test segments in a linked list
 	 * Segments run 10-20-45-89-10
 	 * @return
+	 * @throws ProfileException 
 	 */
-	private List<IBorderSegment> makeTestSegments(){
+	private List<IBorderSegment> makeTestSegments() throws ProfileException{
 		List<IBorderSegment> list = new ArrayList<IBorderSegment>();
 		
 		
-		DefaultBorderSegment p1 = new DefaultBorderSegment(10, 20, 100, UUID.fromString("00000000-0000-0000-0000-000000000000"));
-		DefaultBorderSegment p2 = new DefaultBorderSegment(20, 45, 100, UUID.fromString("11111111-1111-1111-1111-111111111111"));
-		DefaultBorderSegment p3 = new DefaultBorderSegment(45, 89, 100, UUID.fromString("22222222-2222-2222-2222-222222222222"));
-		DefaultBorderSegment p4 = new DefaultBorderSegment(89, 10, 100, UUID.fromString("33333333-3333-3333-3333-333333333333"));
+		DefaultBorderSegment p1 = new DefaultBorderSegment(10, 20, 100, UUID.fromString(SEG_0));
+		DefaultBorderSegment p2 = new DefaultBorderSegment(20, 45, 100, UUID.fromString(SEG_1));
+		DefaultBorderSegment p3 = new DefaultBorderSegment(45, 89, 100, UUID.fromString(SEG_2));
+		DefaultBorderSegment p4 = new DefaultBorderSegment(89, 10, 100, UUID.fromString(SEG_3));
 
 		list.add(p1);
 		list.add(p2);
 		list.add(p3);
 		list.add(p4);
-		try {
-			IBorderSegment.linkSegments(list);
-		} catch (ProfileException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		IBorderSegment.linkSegments(list);
+
 		return list;
 	}
 	
 	/**
 	 * Creata a test profile with segments as in {@link #makeTestSegments()}
 	 * @return
+	 * @throws ProfileException 
 	 */
-	private ISegmentedProfile makeTestProfile(){
+	private ISegmentedProfile makeTestProfile() throws ProfileException{
 		
 		List<IBorderSegment> list = makeTestSegments();
 		IProfile profile = new FloatProfile(10, 100);
-		try {
-			return new SegmentedFloatProfile(profile, list);
-		} catch (ProfileException e) {
-
-			e.printStackTrace();
-			return null;
-		}
+		return new SegmentedFloatProfile(profile, list);
 	}
 	
 	@Rule
 	public final ExpectedException exception = ExpectedException.none();
 
 	@Test
-	public void testReverse() {
-		fail("Not yet implemented");
+	public void testReverse() throws ProfileException {
+	    ISegmentedProfile testProfile = makeTestProfile();
+	    sp.reverse();
+	    
+	    for(int i=0, j=sp.getSegmentCount()-1; i<sp.getSegmentCount(); i++, j--){
+	        IBorderSegment test   = testProfile.getSegmentAt(i);
+	        IBorderSegment result = sp.getSegmentAt(j);
+	        
+	        assertEquals(test.getID(), result.getID());
+	        assertEquals(test.length(), result.length()); 
+	    } 
 	}
 
 	@Test
@@ -172,53 +165,73 @@ public class SegmentedFloatProfileTest {
 
 	@Test
 	public void testGetSegmentString() {
-		fail("Not yet implemented");
+	    IBorderSegment test = new DefaultBorderSegment(20, 45, 100, UUID.fromString(SEG_1));
+	    String name = "Seg_1";
+	    IBorderSegment result = sp.getSegment(name);
+	    assertEquals(test.getID(), result.getID());
+        assertEquals(test.getStartIndex(), result.getStartIndex());
+        assertEquals(test.getEndIndex(), result.getEndIndex());
 	}
+	
+	@Test
+    public void testGetSegmentStringExceptsWithNullInput(){
+        exception.expect(IllegalArgumentException.class);
+        sp.getSegment( (String)null);
+    }
 
 	@Test
 	public void testGetSegmentIBorderSegment() {
-		fail("Not yet implemented");
+	    IBorderSegment test = new DefaultBorderSegment(20, 45, 100, UUID.fromString(SEG_1));
+	    IBorderSegment result = sp.getSegment(test);
+	    assertEquals(test.getID(), result.getID());
+	    assertEquals(test.getStartIndex(), result.getStartIndex());
+	    assertEquals(test.getEndIndex(), result.getEndIndex());
 	}
+	
+	@Test
+    public void testGetSegmentIBorderSegmentExceptsWithNullInput(){
+        exception.expect(IllegalArgumentException.class);
+        sp.getSegment( (IBorderSegment)null);
+    }
 
 	@Test
 	public void testGetSegmentAt() {
-		fail("Not yet implemented");
+	    IBorderSegment s = sp.getSegmentAt(1);
+	    assertEquals(UUID.fromString(SEG_1), s.getID());
+	}
+	
+	@Test
+	public void testGetSegmentAtExceptsWithInvalidInputLow(){
+	    exception.expect(IllegalArgumentException.class);
+	    sp.getSegmentAt(-1);
+	}
+	
+	@Test
+	public void testGetSegmentAtExceptsWithInvalidInputHigh(){
+	    exception.expect(IllegalArgumentException.class);
+	    sp.getSegmentAt(5);
 	}
 
 	@Test
-	public void testGetSegmentContaining() {
-		ISegmentedProfile sp = makeTestProfile();
+	public void testGetSegmentContaining() throws ProfileException {
 //		10-20-45-89-10
 		IBorderSegment s = sp.getSegmentContaining(0);
-		assertEquals(UUID.fromString("33333333-3333-3333-3333-333333333333"), s.getID());
-		
-		try {
-			
-			sp = sp.offset(25);
-			// Should now be 85-95-20-64-85
-			 s = sp.getSegmentContaining(0);
-			assertEquals(UUID.fromString("11111111-1111-1111-1111-111111111111"), s.getID());
-			
-			
-		} catch (ProfileException e) {
-			fail("Error in test");
-			e.printStackTrace();
-		}
-		
-		testGetSegmentContainingStartOutOfBounds();
-		testGetSegmentContainingEndOutOfBounds();
+		assertEquals(UUID.fromString(SEG_3), s.getID());
+
+		sp = sp.offset(25);
+		// Should now be 85-95-20-64-85
+		s = sp.getSegmentContaining(0);
+		assertEquals(UUID.fromString(SEG_1), s.getID());
 	}
 	
 	@Test
 	public void testGetSegmentContainingStartOutOfBounds(){
-		ISegmentedProfile sp = makeTestProfile();
 		exception.expect(IllegalArgumentException.class);
 		sp.getSegmentContaining(-5);
 	}
 	
 	@Test
 	public void testGetSegmentContainingEndOutOfBounds(){
-		ISegmentedProfile sp = makeTestProfile();
 		exception.expect(IllegalArgumentException.class);
 		sp.getSegmentContaining(100);
 	}
@@ -279,50 +292,40 @@ public class SegmentedFloatProfileTest {
 	}
 
 	@Test
-	public void testOffsetInt() {
-
-		try{
-			/*
-			 * Complete profile of segments
-			 */
-			List<IBorderSegment> list = makeTestSegments();
-			IBorderSegment p1 = list.get(0);
-			IBorderSegment p2 = list.get(1);
-			IBorderSegment p3 = list.get(2);
-			IBorderSegment p4 = list.get(3);
+	public void testOffsetInt() throws SegmentUpdateException, ProfileException, UnavailableComponentException {
+	    /*
+	     * Complete profile of segments
+	     */
+	    List<IBorderSegment> list = makeTestSegments();
+	    IBorderSegment p1 = list.get(0);
+	    IBorderSegment p2 = list.get(1);
+	    IBorderSegment p3 = list.get(2);
+	    IBorderSegment p4 = list.get(3);
 
 
-			p1.update(5, 20);
-			assertEquals(5, p1.getStartIndex());
-			assertEquals(5, p4.getEndIndex());
+	    p1.update(5, 20);
+	    assertEquals(5, p1.getStartIndex());
+	    assertEquals(5, p4.getEndIndex());
 
-			p4.update(89, 1);
-			assertEquals(1, p1.getStartIndex());
-			assertEquals(1, p4.getEndIndex());
+	    p4.update(89, 1);
+	    assertEquals(1, p1.getStartIndex());
+	    assertEquals(1, p4.getEndIndex());
 
-			/*
-			 * Can the profile be offset and still have segments adjusted?
-			 */
-			ISegmentedProfile sp = makeTestProfile();
+	    /*
+	     * Can the profile be offset and still have segments adjusted?
+	     */
 
-			sp = sp.offset(1);
-			// Should now be 9-19-44-88-9
-			assertEquals(9, sp.getSegment(p1.getID()).getStartIndex());
-			assertEquals(9, sp.getSegment(p4.getID()).getEndIndex());
-			
-			sp = sp.offset(80);
-			// Should now be 29-39-64-8-29
-			assertEquals(29, sp.getSegment(p1.getID()).getStartIndex());
-			assertEquals(39, sp.getSegment(p2.getID()).getStartIndex());
-			assertEquals(64, sp.getSegment(p3.getID()).getStartIndex());
-			assertEquals(8, sp.getSegment(p4.getID()).getStartIndex());
-			
-			
+	    sp = sp.offset(1);
+	    // Should now be 9-19-44-88-9
+	    assertEquals(9, sp.getSegment(p1.getID()).getStartIndex());
+	    assertEquals(9, sp.getSegment(p4.getID()).getEndIndex());
 
-		} catch(SegmentUpdateException | ProfileException | UnavailableComponentException e){
-			fail("Error updating segments");
-			e.printStackTrace();
-		}
+	    sp = sp.offset(80);
+	    // Should now be 29-39-64-8-29
+	    assertEquals(29, sp.getSegment(p1.getID()).getStartIndex());
+	    assertEquals(39, sp.getSegment(p2.getID()).getStartIndex());
+	    assertEquals(64, sp.getSegment(p3.getID()).getStartIndex());
+	    assertEquals(8, sp.getSegment(p4.getID()).getStartIndex());
 	}
 
 	@Test
