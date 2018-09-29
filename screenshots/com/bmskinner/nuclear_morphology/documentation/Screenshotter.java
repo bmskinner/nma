0f44599b1717fd96319ce2cdb71e20be7fcb723d
unknown <bms41@cam.ac.uk>
2018-09-29 15:25:55 +0100

    Added custom crop to screenshotter

--- screenshots/com/bmskinner/nuclear_morphology/documentation/Screenshotter.java
+++ screenshots/com/bmskinner/nuclear_morphology/documentation/Screenshotter.java
@@ -9,19 +9,20 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
 
-import com.bmskinner.nuclear_morphology.core.DatasetListManager;
+import com.bmskinner.nuclear_morphology.TestResources;
+import com.bmskinner.nuclear_morphology.components.generic.Version;
 import com.bmskinner.nuclear_morphology.core.EventHandler;
 import com.bmskinner.nuclear_morphology.core.InputSupplier;
 import com.bmskinner.nuclear_morphology.gui.DefaultInputSupplier;
 import com.bmskinner.nuclear_morphology.gui.events.SignalChangeEvent;
 import com.bmskinner.nuclear_morphology.gui.main.DockableMainWindow;
-import com.bmskinner.nuclear_morphology.gui.tabs.DatasetSelectionListener.DatasetSelectionEvent;
 import com.bmskinner.nuclear_morphology.gui.tabs.DetailPanel;
 import com.bmskinner.nuclear_morphology.io.Io;
 import com.javadocking.dock.TabDock;
@@ -47,10 +48,12 @@ public class Screenshotter {
 	
 	private static final String SCREENSHOT_FOLDER = "screens/";
 	private static final String NULL_DATASET_FOLDER = "Blank";
-	private static final String SINGLE_DATASET_FOLDER = "Single";
+	private static final String SINGLE_ROUND_DATASET_FOLDER = "Single_round";
+	private static final String SINGLE_MOUSE_DATASET_FOLDER = "Single_mouse";
 	private static final String MULTI_DATASET_FOLDER = "Multi";
 	
-	private static final String SINGLE_DATASET = "test/samples/images/Testing_round/UnitTest_1.14.0/UnitTest_1.14.0.nmd";
+	private static final String TEST_MOUSE_DATASET = "test/samples/images/Mouse/UnitTest_"+Version.currentVersion()+"/Mouse.nmd";
+	private static final String TEST_ROUND_DATASET = "test/samples/images/Round_with_signals/UnitTest_"+Version.currentVersion()+"/Round_with_signals.nmd";
 	
 	private final Robot robot;
 	
@@ -94,23 +97,27 @@ public class Screenshotter {
 		InputSupplier is = new DefaultInputSupplier();
 		EventHandler eh = new EventHandler(is);
 		mw = new DockableMainWindow(true,eh);
-//		mw.setExtendedState( mw.getExtendedState()|JFrame.MAXIMIZED_BOTH);
 		mw.setVisible(true);
 
 		Thread.sleep(LOAD_TIME_MILLIS);
 		takeScreens(rootFolder, NULL_DATASET_FOLDER);
 
-		eh.eventReceived(new SignalChangeEvent(this, SignalChangeEvent.IMPORT_DATASET_PREFIX+SINGLE_DATASET, this.getClass().getSimpleName()));
+		eh.eventReceived(new SignalChangeEvent(this, SignalChangeEvent.IMPORT_DATASET_PREFIX+new File(TEST_ROUND_DATASET).getAbsolutePath(), this.getClass().getSimpleName()));
 
 		Thread.sleep(LOAD_TIME_MILLIS);
 
-		takeScreens(rootFolder, SINGLE_DATASET_FOLDER);
+		takeScreens(rootFolder, SINGLE_ROUND_DATASET_FOLDER);
+		
+		eh.eventReceived(new SignalChangeEvent(this, SignalChangeEvent.IMPORT_DATASET_PREFIX+new File(TEST_MOUSE_DATASET).getAbsolutePath(), this.getClass().getSimpleName()));
+		Thread.sleep(LOAD_TIME_MILLIS);
+		takeScreens(rootFolder, SINGLE_MOUSE_DATASET_FOLDER);
 		
 		System.out.println("Select multiple datasets");
 		for(int i=5; i>=0; i--) {
 			System.out.println("Remaining: "+i+"...");
 			Thread.sleep(1000);
 		}
+		
 		// TODO - make populations panel repaint properly
 //		mw.datasetSelectionEventReceived(new DatasetSelectionEvent(this, DatasetListManager.getInstance().getAllDatasets()));
 		takeScreens(rootFolder, MULTI_DATASET_FOLDER);
@@ -125,8 +132,11 @@ public class Screenshotter {
 	 */
 	private Rectangle makeCroppedBounds(Component c) {
 		Rectangle r = c.getBounds();
-		int crop = 9;
-		return new Rectangle(r.x+crop, r.y+crop, r.width-(crop*2), r.height-(crop*2));
+		int topCrop = 6;
+		int btmCrop = 8;
+		int leftCrop = 8;
+		int rightCrop = 8;
+		return new Rectangle(r.x+leftCrop, r.y+topCrop, r.width-(rightCrop+leftCrop), r.height-(topCrop+btmCrop));
 	}
 
 	private void takeScreens(File rootFolder, String prefix) {
@@ -167,7 +177,7 @@ public class Screenshotter {
 	private void exportDetailPanel(DetailPanel d, File folder, String fileNamePrefix) throws AWTException, IllegalArgumentException, IllegalAccessException, InterruptedException, IOException {
 		
 		Field subPanelField = null;
-		Field[] fields = d.getClass().getDeclaredFields();
+		List<Field> fields = getInheritedPrivateFields(d.getClass());
 		
 		for(Field f : fields) {
 			f.setAccessible(true);
@@ -175,21 +185,34 @@ public class Screenshotter {
 			if(f.get(d) instanceof DetailPanel)
 				exportDetailPanel((DetailPanel) f.get(d), folder, fileNamePrefix );
 			
-			if(f.get(d) instanceof JTabbedPane) 
+			if(f.get(d) instanceof JTabbedPane) {
 				subPanelField = f;
+				subPanelField.setAccessible(true);
+				JTabbedPane subPanel = (JTabbedPane) subPanelField.get(d);
+				int tabs = subPanel.getTabCount();
+				for(int j=0; j<tabs; j++) {
+					subPanel.setSelectedIndex(j);
+					Thread.sleep(SLEEP_TIME_MILLIS);
+					takeScreenShot(folder, fileNamePrefix+"_"+j+"_"+subPanel.getTitleAt(j));
+				}	
+			}
 		}
-		
-		if(subPanelField==null)
-			return;
+	}
+	
+	/**
+	 * Get all the private fields for the class, including superclass fields
+	 * @param type
+	 * @return
+	 */
+	private List<Field> getInheritedPrivateFields(Class<?> type) {
+	    List<Field> result = new ArrayList<>();
 
-		subPanelField.setAccessible(true);
-		JTabbedPane subPanel = (JTabbedPane) subPanelField.get(d);
-		int tabs = subPanel.getTabCount();
-		for(int j=0; j<tabs; j++) {
-			subPanel.setSelectedIndex(j);
-			Thread.sleep(SLEEP_TIME_MILLIS);
-			takeScreenShot(folder, fileNamePrefix+"_"+j+"_"+subPanel.getTitleAt(j));
-		}		
+	    Class<?> i = type;
+	    while (i != null && i != Object.class) {
+	        Collections.addAll(result, i.getDeclaredFields());
+	        i = i.getSuperclass();
+	    }
+	    return result;
 	}
 	
 	private void takeScreenShot(File folder, String title) throws IOException {
