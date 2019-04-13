464cda1cbe16585f140df9e39670daa7745ab4fe
Ben Skinner <bms41@cam.ac.uk>
2019-04-13 11:28:25 +0100

    Add a generic cellular component XY chart dataset

--- src/com/bmskinner/nuclear_morphology/charting/datasets/NuclearSignalXYDataset.java
+++ src/com/bmskinner/nuclear_morphology/charting/datasets/NuclearSignalXYDataset.java
@@ -3,34 +3,29 @@ package com.bmskinner.nuclear_morphology.charting.datasets;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jfree.data.xy.DefaultXYDataset;
-
 import com.bmskinner.nuclear_morphology.components.nuclear.INuclearSignal;
 import com.bmskinner.nuclear_morphology.components.nuclei.Nucleus;
 
-public class NuclearSignalXYDataset extends DefaultXYDataset {
+/**
+ * An XY dataset mapping signals and nuclei to their XY coordinates
+ * @author ben
+ *
+ */
+public class NuclearSignalXYDataset extends ComponentXYDataset<Nucleus> {
 	
 	private List<List<INuclearSignal>> signalList = new ArrayList<>();
-	private List<List<Nucleus>> nucleusList = new ArrayList<>();
 	
 	public NuclearSignalXYDataset() {
 		super();
 	}
 	
-	public void addSeries(Comparable seriesKey, double[][] data, List<INuclearSignal> signals, List<Nucleus> nuclei) {
-		super.addSeries(seriesKey, data);
+	public void addSeries(Comparable<?> seriesKey, double[][] data, List<INuclearSignal> signals, List<Nucleus> nuclei) {
+		super.addSeries(seriesKey, data, nuclei);
 		signalList.add(signals);
-		nucleusList.add(nuclei);
 	}
 	
-	public INuclearSignal getSignal(Comparable seriesKey, int item) {
+	public INuclearSignal getSignal(Comparable<?> seriesKey, int item) {
 		int seriesIndex = indexOf(seriesKey);
 		return signalList.get(seriesIndex).get(item);
 	}
-	
-	public Nucleus getNucleus(Comparable seriesKey, int item) {
-		int seriesIndex = indexOf(seriesKey);
-		return nucleusList.get(seriesIndex).get(item);
-	}
-
 }
