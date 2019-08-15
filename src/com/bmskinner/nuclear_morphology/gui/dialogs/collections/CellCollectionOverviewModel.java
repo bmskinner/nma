c7fd9fd0b686d4a5381a917a5a44c3d833b15e09
Ben Skinner <bms41@cam.ac.uk>
2019-08-15 21:04:03 +0100

    Synchonise curation dialog methods

--- src/com/bmskinner/nuclear_morphology/gui/dialogs/collections/CellCollectionOverviewModel.java
+++ src/com/bmskinner/nuclear_morphology/gui/dialogs/collections/CellCollectionOverviewModel.java
@@ -50,7 +50,7 @@ public class CellCollectionOverviewModel extends DefaultTableModel {
 	 * Set the selection for all elements
 	 * @param b
 	 */
-	public void setAllSelected(boolean b) {
+	public synchronized void setAllSelected(boolean b) {
 		for (int r = 0; r < getRowCount(); r++) 
             for (int c = 0; c <getColumnCount(); c++) 
                 setSelected(r, c, b);
@@ -62,7 +62,7 @@ public class CellCollectionOverviewModel extends DefaultTableModel {
 	 * @param c the cell to update
 	 * @param b the selection state
 	 */
-	public void setSelected(ICell c, boolean b) {
+	public synchronized void setSelected(ICell c, boolean b) {
 		for (int row = 0; row < getRowCount(); row++) {
 			for (int col = 0; col < getColumnCount(); col++) {
 				if(getValueAt(row, col)==c) {
@@ -78,7 +78,7 @@ public class CellCollectionOverviewModel extends DefaultTableModel {
 	 * @param r
 	 * @param c
 	 */
-	public void toggleSelected(int r, int c) {
+	public synchronized void toggleSelected(int r, int c) {
 		Object o = getValueAt(r, c);
 		setSelected(r, c, !selected.contains(o));
 	}
@@ -89,7 +89,7 @@ public class CellCollectionOverviewModel extends DefaultTableModel {
 	 * @param c
 	 * @param b
 	 */
-	public void setSelected(int r, int c, boolean b) {
+	public synchronized void setSelected(int r, int c, boolean b) {
 
 		SelectableCellIcon icon = (SelectableCellIcon)getValueAt(r, c);
 		if(icon.getCell()==null) // don't select empty table rows
