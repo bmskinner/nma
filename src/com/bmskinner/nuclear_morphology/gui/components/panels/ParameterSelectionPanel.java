7cf131052ff38d2acf867955ac5e4774821506ba
Ben Skinner <b.skinner@essex.ac.uk>
2019-08-21 17:03:41 +0100

    Move texture features in clustering setup panel

--- src/com/bmskinner/nuclear_morphology/gui/components/panels/ParameterSelectionPanel.java
+++ src/com/bmskinner/nuclear_morphology/gui/components/panels/ParameterSelectionPanel.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -15,6 +16,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.bmskinner.nuclear_morphology.analysis.image.GLCM.GLCMValue;
 import com.bmskinner.nuclear_morphology.analysis.profiles.ProfileException;
 import com.bmskinner.nuclear_morphology.components.IAnalysisDataset;
 import com.bmskinner.nuclear_morphology.components.generic.ProfileType;
@@ -53,11 +55,16 @@ public class ParameterSelectionPanel extends OptionsPanel {
 		
 		JPanel stats = createStatsPanel();
 		stats.setAlignmentY(Component.TOP_ALIGNMENT);
+		
+		JPanel texture = createTexturePanel();
+		texture.setAlignmentY(Component.TOP_ALIGNMENT);
 
 		paramPanel.add(shape);
 		paramPanel.add(Box.createHorizontalGlue());
 		paramPanel.add(stats);
-		
+		paramPanel.add(Box.createHorizontalGlue());
+		paramPanel.add(texture);
+
 		panel.add(paramPanel);
 		panel.setBorder(BorderFactory.createTitledBorder("Parameters"));
 		return panel;
@@ -72,7 +79,7 @@ public class ParameterSelectionPanel extends OptionsPanel {
 			options.setBoolean(t.toString(), false);
 		
 		options.setBoolean(ProfileType.ANGLE.toString(), true);
-
+		
 		try {
 			for (IBorderSegment s : dataset.getCollection().getProfileCollection().getSegments(Tag.REFERENCE_POINT))
 				options.setBoolean(s.getID().toString(), false);
@@ -93,6 +100,12 @@ public class ParameterSelectionPanel extends OptionsPanel {
 		for (PlottableStatistic stat : PlottableStatistic.getNucleusStats((dataset.getCollection().getNucleusType()))) {
 			if(stat.getDimension().equals(StatisticDimension.DIMENSIONLESS))
 				continue;
+			
+			// Handle texture separately
+			PlottableStatistic[] textureStats = GLCMValue.toStats();
+			if(Arrays.stream(textureStats).anyMatch(s->s.equals(stat)))
+				continue;
+			
 			JCheckBox box = new JCheckBox();
 			box.addChangeListener(e ->  options.setBoolean(stat.toString(), box.isSelected()));
 			box.setForeground(Color.DARK_GRAY);
@@ -126,6 +139,34 @@ public class ParameterSelectionPanel extends OptionsPanel {
 		for (PlottableStatistic stat : PlottableStatistic.getNucleusStats((dataset.getCollection().getNucleusType()))) {
 			if(!stat.getDimension().equals(StatisticDimension.DIMENSIONLESS))
 				continue;
+			
+			// Handle texture separately
+			PlottableStatistic[] textureStats = GLCMValue.toStats();
+			if(Arrays.stream(textureStats).anyMatch(s->s.equals(stat)))
+				continue;
+
+			JCheckBox box = new JCheckBox();
+			box.addChangeListener(e ->  options.setBoolean(stat.toString(), box.isSelected()));
+			box.setForeground(Color.DARK_GRAY);
+			box.setSelected(options.getBoolean(stat.toString()));
+			JLabel label = new JLabel(stat.toString());
+			labels.add(label);
+			fields.add(box);
+		}
+		
+		addLabelTextRows(labels, fields, layout, panel);
+		return panel;
+	}
+	
+	private JPanel createTexturePanel() {
+		JPanel panel = new JPanel();
+		GridBagLayout layout = new GridBagLayout();
+		panel.setLayout(layout);
+
+		List<JLabel> labels = new ArrayList<>();
+		List<Component> fields = new ArrayList<>();
+		
+		for (PlottableStatistic stat : GLCMValue.toStats()) {
 			JCheckBox box = new JCheckBox();
 			box.addChangeListener(e ->  options.setBoolean(stat.toString(), box.isSelected()));
 			box.setForeground(Color.DARK_GRAY);
