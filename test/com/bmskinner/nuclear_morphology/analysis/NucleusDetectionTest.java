7cdd02cb5aa6ca642e74d207819e756badcd242c
Ben Skinner <b.skinner@essex.ac.uk>
2019-12-18 15:47:21 +0000

    Add new class for testing overall nucleus parameter detection

---
+++ test/com/bmskinner/nuclear_morphology/analysis/NucleusDetectionTest.java
@@ -0,0 +1,14 @@
+package com.bmskinner.nuclear_morphology.analysis;
+
+/**
+ * Test that nuclei in an image are detected,
+ * and all parameters are calculated. For mouse
+ * sperm, this includes the hook length and body
+ * width
+ * @author bs19022
+ * @since 1.17.1
+ *
+ */
+public class NucleusDetectionTest {
+
+}
