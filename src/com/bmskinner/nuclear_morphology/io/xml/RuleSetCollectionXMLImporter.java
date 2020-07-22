d1e4f063ede1767cf7d298ec7613960b11212dea
Ben Skinner <b.skinner@essex.ac.uk>
2020-07-22 19:09:42 +0100

    Add importer for ruleset XML files

---
+++ src/com/bmskinner/nuclear_morphology/io/xml/RuleSetCollectionXMLImporter.java
@@ -0,0 +1,45 @@
+package com.bmskinner.nuclear_morphology.io.xml;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.jdom2.Document;
+import org.jdom2.input.SAXBuilder;
+
+import com.bmskinner.nuclear_morphology.components.rules.RuleSetCollection;
+import com.bmskinner.nuclear_morphology.io.Io.Importer;
+import com.bmskinner.nuclear_morphology.io.xml.XMLReader.XMLReadingException;
+
+/**
+ * Import XML files containing ruleset descriptions
+ * @author ben
+ * @since 1.18.3
+ *
+ */
+public class RuleSetCollectionXMLImporter implements Importer {
+	
+	private static final Logger LOGGER = Logger.getLogger(RuleSetCollectionXMLImporter.class.getName());
+	
+	private final File file;
+	
+	public RuleSetCollectionXMLImporter(@NonNull final File f) {
+		file = f;
+	}
+
+	public RuleSetCollection importRuleset() throws XMLReadingException {
+		try {
+	         SAXBuilder saxBuilder = new SAXBuilder();
+	         Document document = saxBuilder.build(file);
+	         
+	         RuleSetCollectionXMLReader reader = new RuleSetCollectionXMLReader(document.getRootElement());
+	         return reader.read();
+	         
+		} catch(Exception e) {
+			LOGGER.log(Level.SEVERE, "Error reading "+file.getAbsolutePath()+": "+e.getMessage(), e);
+			throw new XMLReadingException("Unable to read ruleset file", e);
+		}
+	}
+	
+}
