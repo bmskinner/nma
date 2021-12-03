93ee9c058c94018af649f8e03a2326e764755046
Ben Skinner <b.skinner@essex.ac.uk>
2021-12-03 19:36:40 +0000

    Add array utility class for reversing an array

---
+++ src/com/bmskinner/nuclear_morphology/utility/ArrayUtils.java
@@ -0,0 +1,19 @@
+package com.bmskinner.nuclear_morphology.utility;
+
+public class ArrayUtils {
+	
+	/**
+	 * Reverse the given array
+	 * @param arr
+	 * @return
+	 */
+	public static int[] reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int t = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = t;
+        }
+        return arr;
+	}
+
+}
