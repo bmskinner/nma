1c5ea7e707fa2bacba8117023a1b0a0c46264f25
Ben Skinner <b.skinner@essex.ac.uk>
2022-06-01 13:23:57 +0100

    Fix issues rendering user guide

--- templates/renderUserGuide.bat
+++ templates/renderUserGuide.bat
@@ -1,3 +1,5 @@
+
+
 REM Render the user guide from markdown
 Rscript -e "bookdown::render_book('index.Rmd', clean = FALSE)"
 
@@ -6,4 +8,5 @@ Xcopy /E /I ".\\_book" "..\\..\\target\\classes\\user-guide"
 
 REM Delete the existing installed user guide in the nma folder so we can
 REM test if the packaged version is loading properly
-rmdir /Q /S ${user.home}\\.nma\\user_guide_v${project.version}
+SET F="${user.home}\\.nma\\user_guide_v${project.version}"
+if exist %f% rmdir /Q /S %f%
