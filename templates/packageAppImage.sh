f0979f41f4b17d48cf3c452e3b45baff6ec25ea6
Ben Skinner <b.skinner@essex.ac.uk>
2022-06-01 16:41:32 +0100

    Add launcher shell script to linux package and improve folder structure

--- templates/packageAppImage.sh
+++ templates/packageAppImage.sh
@@ -5,13 +5,20 @@
 # i.e. invoke /mnt/nma/scripts/packageAppImage.sh
 
 # Move the files to a local directory in the VM
+rm -rf ~/Documents/linux
 cp -r /mnt/nma/target/linux ~/Documents/linux
 cd ~/Documents/linux
 
 # Package
 jpackage --name "Nuclear Morphology Analysis" --app-version ${project.version} --icon ./res/icons/icon.png --input ./standalone --dest ./appimage --type app-image --main-jar ${jar.finalName}_standalone.jar --main-class com.bmskinner.nma.core.NuclearMorphologyAnalysis
 
-# Make a tar
-tar -czvf Nuclear_Morphology_Analysis_${project.version}.tar.gz ./appimage/Nuclear\ Morphology\ Analysis
+# Create the launcher script
+cd ~/Documents/linux/appimage
 
-cp Nuclear_Morphology_Analysis_${project.version}.tar.gz /mnt/nma/target/Nuclear_Morphology_Analysis-${project.version}-x86_64-linux.tar.gz
+echo "./bin/Nuclear\ Morphology\ Analysis" > ./Nuclear\ Morphology\ Analysis/launch.sh
+chmod +x ./Nuclear\ Morphology\ Analysis/launch.sh
+
+# Make a tar
+tar -czf /mnt/nma/target/Nuclear_Morphology_Analysis-${project.version}-x86_64-linux.tar.gz ./Nuclear\ Morphology\ Analysis
+
+#cp Nuclear_Morphology_Analysis_${project.version}.tar.gz /mnt/nma/target/Nuclear_Morphology_Analysis-${project.version}-x86_64-linux.tar.gz
