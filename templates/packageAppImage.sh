13d52571bfbe61f6ef22afa87466924a71096e32
Ben Skinner <b.skinner@essex.ac.uk>
2023-03-24 12:55:58 +0000

    Update jpackage commands to include beta suffixes when needed

--- templates/packageAppImage.sh
+++ templates/packageAppImage.sh
@@ -2,11 +2,11 @@
 # Create a portable app image that does not require installation
 # Package
 
-jpackage --name "Nuclear Morphology Analysis" --app-version ${project.version} --icon ${project.basedir}/res/icons/icon.png --input ${project.basedir}/target/standalone --dest ${project.basedir}/target/appimage/Nuclear\ Morphology\ Analysis --type app-image --main-jar ${jar.finalName}_standalone.jar --main-class com.bmskinner.nma.core.NuclearMorphologyAnalysis
+jpackage --name "Nuclear Morphology Analysis" --app-version ${project.version} --icon ${project.basedir}/res/icons/icon.png --input ${project.basedir}/target/standalone --dest ${project.basedir}/target/appimage/Nuclear\ Morphology\ Analysis --type app-image --main-jar ${jar.finalName}.jar --main-class com.bmskinner.nma.core.NuclearMorphologyAnalysis
 
 # Create a launcher script
 echo "./bin/Nuclear\ Morphology\ Analysis" > ${project.basedir}/target/appimage/Nuclear\ Morphology\ Analysis/launch.sh
 chmod +x ${project.basedir}/target/appimage/Nuclear\ Morphology\ Analysis/launch.sh
 
 # Make a zipped tar
-tar -czf ${project.basedir}/target/Nuclear_Morphology_Analysis-${project.version}-linux.tar.gz -C ${project.basedir}/target/appimage Nuclear\ Morphology\ Analysis 
+tar -czf ${project.basedir}/target/${jar.finalName}-linux.tar.gz -C ${project.basedir}/target/appimage Nuclear\ Morphology\ Analysis 
--- templates/uploadFilesToBitbucket.bat
+++ templates/uploadFilesToBitbucket.bat
@@ -1,3 +1,3 @@
 REM Upload files to NMA downloads section on Bitbucket
 
-curl -s -u bmskinner -X POST https://api.bitbucket.org/2.0/repositories/bmskinner/nuclear_morphology/downloads -F files=@..\target\Nuclear_Morphology_Analysis-${project.version}-windows.msi  -F files=@..\target\Nuclear_Morphology_Analysis-${project.version}-windows.zip -F files=@..\target\Nuclear_Morphology_Analysis-${project.version}-linux.tar.gz -F files=@..\target\standalone\Nuclear_Morphology_Analysis_${project.version}_standalone.jar
+curl -s -u bmskinner -X POST https://api.bitbucket.org/2.0/repositories/bmskinner/nuclear_morphology/downloads -F files=@..\target\\${jar.finalName}-windows.msi  -F files=@..\target\\${jar.finalName}-windows.zip -F files=@..\target\\${jar.finalName}-linux.tar.gz -F files=@..\target\standalone\\${jar.finalName}.jar
