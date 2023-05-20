9e0de0e6054382f846e13e24b19f42b0730dd753
Ben Skinner <b.skinner@essex.ac.uk>
2023-05-20 12:54:10 +0100

    Fix issue packaging Linux appimage

--- templates/packageAppImage.sh
+++ templates/packageAppImage.sh
@@ -2,10 +2,10 @@
 # Create a portable app image that does not require installation
 # Package
 
-jpackage --name "Nuclear Morphology Analysis" --app-version ${project.version} --icon ${project.basedir}/res/icons/icon.png --input ${project.basedir}/target/standalone --dest ${project.basedir}/target/appimage/Nuclear\ Morphology\ Analysis --type app-image --main-jar ${jar.finalName}.jar --main-class com.bmskinner.nma.core.NuclearMorphologyAnalysis
+jpackage --name "Nuclear Morphology Analysis" --app-version ${project.version} --icon ${project.basedir}/res/icons/icon.png --input ${project.basedir}/target/standalone --dest ${project.basedir}/target/appimage --type app-image --main-jar ${jar.finalName}.jar --main-class com.bmskinner.nma.core.NuclearMorphologyAnalysis
 
 # Create a launcher script
-echo "./bin/Nuclear\ Morphology\ Analysis" > ${project.basedir}/target/appimage/Nuclear\ Morphology\ Analysis/launch.sh
+printf "#!/bin/bash\n./bin/Nuclear\ Morphology\ Analysis" > ${project.basedir}/target/appimage/Nuclear\ Morphology\ Analysis/launch.sh
 chmod +x ${project.basedir}/target/appimage/Nuclear\ Morphology\ Analysis/launch.sh
 
 # Make a zipped tar
