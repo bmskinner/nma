6356d3bfaad2aed4a8422b0d23ab93897766f13d
Ben Skinner <b.skinner@essex.ac.uk>
2023-05-23 13:58:43 +0100

    Ensure separate appimage directory for Windows and Linux

--- templates/packageAppImage.bat
+++ templates/packageAppImage.bat
@@ -1,8 +1,8 @@
 REM Create the portable app image for Windows
-jpackage --name "Nuclear Morphology Analysis" --app-version ${project.version} --icon ..\res\icons\icon.ico --input ..\target\standalone --dest ..\target\appimage --type app-image --description "Morphometric analysis software" --vendor "Ben Skinner" --copyright "Ben Skinner 2015-${build.year}" --main-jar ${jar.finalName}.jar --main-class com.bmskinner.nma.core.NuclearMorphologyAnalysis
+jpackage --name "Nuclear Morphology Analysis" --app-version ${project.version} --icon ..\res\icons\icon.ico --input ..\target\standalone --dest ..\target\appimage-win --type app-image --description "Morphometric analysis software" --vendor "Ben Skinner" --copyright "Ben Skinner 2015-${build.year}" --main-jar ${jar.finalName}.jar --main-class com.bmskinner.nma.core.NuclearMorphologyAnalysis
 
 REM Zip the app image for Windows portable upload
-7z a -tzip "..\target\\${jar.finalName}-windows.zip" "..\target\appimage\Nuclear Morphology Analysis"
+7z a -tzip "..\target\\${jar.finalName}-windows.zip" "..\target\appimage-win\Nuclear Morphology Analysis"
 
 REM Create windows installer
 jpackage --name "Nuclear Morphology Analysis" --app-version ${project.version} --icon ..\res\icons\icon.ico --input ..\target\standalone --dest ..\target\msi --type msi --description "Morphometric analysis software" --vendor "Ben Skinner" --copyright "Ben Skinner 2015-${build.year}" --win-dir-chooser --win-menu --win-per-user-install --win-menu-group "Nuclear Morphology Analysis" --main-jar ${jar.finalName}.jar --main-class com.bmskinner.nma.core.NuclearMorphologyAnalysis
