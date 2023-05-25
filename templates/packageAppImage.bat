15ba6dfcf50a5a1f8e8ed5d3109ff21fea62c83f
Ben Skinner <b.skinner@essex.ac.uk>
2023-05-25 11:07:00 +0100

    Add standalone jar to packages directory

--- templates/packageAppImage.bat
+++ templates/packageAppImage.bat
@@ -8,3 +8,6 @@ REM Create windows installer
 jpackage --name "Nuclear Morphology Analysis" --app-version ${project.version} --icon ..\res\icons\icon.ico --input ..\target\standalone --dest ..\target\msi --type msi --description "Morphometric analysis software" --vendor "Ben Skinner" --copyright "Ben Skinner 2015-${build.year}" --win-dir-chooser --win-menu --win-per-user-install --win-menu-group "Nuclear Morphology Analysis" --main-jar ${jar.finalName}.jar --main-class com.bmskinner.nma.core.NuclearMorphologyAnalysis
 
 copy "..\target\msi\\Nuclear Morphology Analysis-${project.version}.msi" "..\packages\\${jar.finalName}-windows.msi"
+
+REM Copy the standalone jar to the packages directory
+copy "..\target\\standalone\\${jar.finalName}.jar" "..\packages\\${jar.finalName}.jar"
